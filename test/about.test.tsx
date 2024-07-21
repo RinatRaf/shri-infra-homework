8a8baba4eade6ed863a02d9be40241c7fef0d16b
Rafikov Rinat <rinat.rafikov5@yandex.ru>
2024-07-21 23:37:34 +0500

    финальная проверка

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe("About", () => {
   it("Should render author name", () => {
     const { container } = render(<About />);
 
-    expect(container.textContent).toBe("AboutAuthor: Rafikov ");
+    expect(container.textContent).toBe("AboutAuthor: Rafikov Rinat");
   });
 });
